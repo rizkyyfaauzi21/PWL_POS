c00d925459d5fd484277b58163b9ab3b62595de3
rizkyyfaauzi21 <muhammadrizkyfauzi061102@gmail.com>
2024-03-05 11:35:04 +0700

    Hasil Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,14 @@ class UserController extends Controller
     //
     public function index()
     {
+        // Praktikum 2.2
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
+
+        // Praktikum 2.1
         // $user = UserModel::find(1);
         // return view('user', ['data' => $user]);
 
@@ -22,11 +30,12 @@ class UserController extends Controller
         // $user = UserModel::firstwhere('level_id', 1);
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::findor(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
 
+        // Praktikum 1
         // $data = [
         //     'level_id' => 2,
         //     'username' => 'manager_tiga',
