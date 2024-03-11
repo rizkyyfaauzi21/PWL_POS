0eed112183a43096c92f088cbf49666d18728e8f
rizkyyfaauzi21 <muhammadrizkyfauzi061102@gmail.com>
2024-03-11 12:26:17 +0700

    Hasil Praktikum 2.6

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,6 +8,7 @@
 </head>
 <body>
     <h1>Data User</h1>
+    <a href="{{ route('/user/tambah')}}">Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
@@ -16,15 +17,18 @@
             <th>ID Level Pengguna</th>
             {{-- <th>Jumlah Pengguna</th> --}}
         </tr>
-        {{-- @foreach ($data as $d) --}}
+        @foreach ($data as $d)
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
             {{-- <td>{{ $data }}</td> --}}
+            <td><a href="{{ route('/user/ubah', $d->user_id)}}">Ubah</a> | 
+                <a href="{{ route('/user/hapus', $d->user_id)}}">Hapus</a>
+            </td>
         </tr>
-        {{-- @endforeach --}}
+        @endforeach
     </table>
 </body>
 </html>
