15220759a6c22a181161fd9ad5f6015b9be8d10f
rizkyyfaauzi21 <muhammadrizkyfauzi061102@gmail.com>
2024-04-23 11:33:43 +0700

    Hasil praktikum 2 js 10

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
